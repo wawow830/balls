9286ffe02b7eabbdf302b6f2fc358cc9f12a421d
wawow <213274403+wawow830@users.noreply.github.com>
2026-07-20 01:10:39 +1000

    feat: add deterministic data generator

---
+++ cmd/balls/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/wawow830/balls/internal/sequence"
+)
+
+func main() {
+	count := flag.Int("count", 10, "number of JSON records to emit")
+	seed := flag.Uint64("seed", 1, "deterministic generator seed")
+	flag.Parse()
+
+	if *count < 0 {
+		fmt.Fprintln(os.Stderr, "count must be non-negative")
+		os.Exit(2)
+	}
+
+	output := bufio.NewWriter(os.Stdout)
+	encoder := json.NewEncoder(output)
+	generator := sequence.New(*seed)
+	for range *count {
+		if err := encoder.Encode(generator.Next()); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+	if err := output.Flush(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
